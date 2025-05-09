94e8467877cbbab7f175e01bd92c121a0569b050
Ahmet <ahmet@librasoftworks.com>
2025-05-09 15:46:09 +0300

    Added Tenant Project

---
+++ HRA/back/hra/src/Tenants/Presentation/WebApi/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+app.MapGet("api/Test/", () => "Hello, World!");
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

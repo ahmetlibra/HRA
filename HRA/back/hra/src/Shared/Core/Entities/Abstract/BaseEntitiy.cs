c8a55cbf8281c817990c29225ba9356514a4e470
Ahmet <ahmet@librasoftworks.com>
2025-09-15 16:01:13 +0300

    Fix typo in EntityStatus property name
    Corrected the property name `EntitiyStatus` to `EntityStatus` in the `BaseEntity.cs` file to enhance clarity and correctness.

--- HRA/back/hra/src/Shared/Core/Entities/Abstract/BaseEntitiy.cs
+++ HRA/back/hra/src/Shared/Core/Entities/Abstract/BaseEntitiy.cs
@@ -13,7 +13,7 @@ namespace Core.Entities.Abstract
 
         public Guid TenantId { get; set; }
 
-        public EntityStatus EntitiyStatus { get; set; } = EntityStatus.Pending; 
+        public EntityStatus EntityStatus { get; set; } = EntityStatus.Pending; 
 
         public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.UtcNow; // Ticks yerine DateTimeOffset kullanmak daha standart ve zaman dilimi (timezone) sorunlarını çözer.
         public DateTimeOffset? UpdatedDate { get; set; }
